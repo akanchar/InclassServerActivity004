7b56789427a782640f90d88f664890fd742c4596
devin_mason <mvp@vastofi.com>
2024-10-17 15:19:09 -0400

    FEAT: Implementation of my personal code to avoid merge conflicts

--- result.php
+++ result.php
@@ -1,5 +1,19 @@
 <?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
 
+// Set header for image output
+header('Content-Type: image/jpeg');
 
+// Load the image from the filename passed in the form
+$filename = __DIR__ . '/images/' . $_GET['file'] . '.jpg';
+$image = imagecreatefromjpeg($filename);
 
+// Get the width value from the form (default to 500 if not set)
+$width = isset($_GET['width']) ? $_GET['width'] : 500;
+$newImage = imagescale($image, $width);
+
+// Output the final image
+imagejpeg($newImage);
+imagedestroy($newImage);
 ?>
