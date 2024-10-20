cc594760e679cca0dd12a2f4799fd8f3de7bbbb0
NathanielGonzalez <nathaniel.gonzalez2004@gmail.com>
2024-10-20 13:37:50 -0400

    FEAT - Made changes to form.php (Nathaniel Gonzalez)

--- form.php
+++ form.php
@@ -1,7 +1,5 @@
 <?php
   include 'data.inc.php';
-
-
 ?>
 <!DOCTYPE html>
 <html>
@@ -14,26 +12,44 @@
 </head>
 <body>
     <h1>Fine Art Meme Maker</h1> 
-    <section class="grid-container">
 
-    </section>     
+
+    <section class="grid-container">
+    <?php
+        foreach ($paintings as $painting) {
+
+            echo "<img src='result.php?file=" . $painting['filename'] . ".jpg&width=100' data-value='" . $painting['filename'] . "' />";
+        }
+    ?>
+</section>
+
+
     <form action="result.php" method="get">
         <label>Select Base Painting:</label>
         <select name="file" id="whichPainting">
-
+            <?php
+                foreach ($paintings as $painting) {
+                    echo "<option value='" . $painting['filename'] . "'>" . $painting['title'] . "</option>";
+                }
+            ?>
         </select>
+
         <label>Meme 1 Text:</label>
         <input type="text" name="text1" size=50 value="Default text" />
+        
         <label>Meme 1 Font Size:</label>
-        12 <input type="range" name="size1" min=12 max=64  value=24 /> 64
-        <label>Meme 2 Text:</label>
-        <input type="text" name="text2" size=50 value="More text"/>   
-        <label>Meme 2 Font Size:</label>
-        12 <input type="range" name="size2" min=12 max=48 value=48 /> 48  
-        <label>Image Width:</label>
-        500 <input type="range" name="width" min=500 max=1000  value=500 /> 1000            
+        12 <input type="range" name="size1" min=12 max=64 value=24 /> 64
 
-        <input type="submit" value="Click To See Meme" >
+        <label>Meme 2 Text:</label>
+        <input type="text" name="text2" size=50 value="More text" />
+        
+        <label>Meme 2 Font Size:</label>
+        12 <input type="range" name="size2" min=12 max=48 value=48 /> 48
+
+        <label>Image Width:</label>
+        500 <input type="range" name="width" min=500 max=1000 value=500 /> 1000
+
+        <input type="submit" value="Click To See Meme">
     </form>
    
 </body>
